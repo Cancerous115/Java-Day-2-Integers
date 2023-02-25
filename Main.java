3ab98ae43139d2884583108e04187ad7583c06d8
Cancerous115 <107626947+Cancerous115@users.noreply.github.com>
2023-02-25 18:06:03 +0000

    Added new code

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
-class Main {
-  static int age;
-  public static void main(String[] args) {
-    int age =30;
-    System.out.println("Hello world!"+ "I am"+age+"years old");
+class Main{
+  public static void main(String[]args){
+    double number1=7.9;
+    int number2=(int)number1;
+    System.out.println(number2);
   }
 }
